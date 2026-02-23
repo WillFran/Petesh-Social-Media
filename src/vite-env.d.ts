c29b2d55d09bb7c4d4808551197c32fed36911b7
Willy Delgado <delgado030890@gmail.com>
2026-02-22 19:50:15 -0500

    Fix Vite env typing (rename vite-env.d.ts)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_ANON_KEY: string
+  // agrega aquí otras si las usas:
+  // readonly VITE_CLOUDINARY_CLOUD_NAME: string
 }
 
 interface ImportMeta {
